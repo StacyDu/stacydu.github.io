7aeffc90b0c2fd8fb7ebd740b6533809a32c724d
StacyDu <dudinaanastasiia@gmail.com>
2019-08-18 20:25:28 +0200

    Projects section. JS code

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,22 @@
 "use strict";
 
-// place your code below
+const list = document.querySelector('.projects-list--js');
 
-
-console.log(`Hello world!`);
+fetch('https://api.github.com/users/stacydu/repos?sort=update&direction=desc')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for (const repo of repos) {
+      const {html_url, name} = repo;
+      list.innerHTML += `
+      <li class='projects-list__item'>
+        <a class='list__link' href='${html_url}'>${name}</a>
+      </li>
+      `;
+    }
+  })
+  .catch (err => {
+    console.log(err);
+  })
 
 
